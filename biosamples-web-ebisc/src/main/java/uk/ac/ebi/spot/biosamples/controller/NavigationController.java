71c9a129bb7843808aa879d38ad59b4e1d9f30a4
Kerruba <luca.cherubin@gmail.com>
2016-04-27 17:17:58 +0100

    Create the EBiSC webinterface project

---
+++ biosamples-web-ebisc/src/main/java/uk/ac/ebi/spot/biosamples/controller/NavigationController.java
@@ -0,0 +1,46 @@
+package uk.ac.ebi.spot.biosamples.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import uk.ac.ebi.spot.biosamples.model.Breadcrumb;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Javadocs go here!
+ *
+ * @author lucacherubin
+ * @date 18/03/2016
+ */
+@Controller
+public class NavigationController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "home";
+    }
+
+    @RequestMapping("/search")
+    public String search(Model model, String searchTerm, HttpServletRequest request) {
+        model.addAttribute("searchTerm",searchTerm);
+        return "search";
+    }
+
+    @RequestMapping("/samples")
+    public String samples() {
+        return "samples_home";
+    }
+
+    @RequestMapping("/groups")
+    public String groups() {
+        return "groups_home";
+    }
+
+
+    @RequestMapping("/about")
+    public String about() {
+        return "about";
+    }
+}
