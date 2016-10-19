3a87dc481ab3b1acac553d0d15b74867fd457001
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-10-19 16:52:13 +0100

    expanded ingores relationship fields

--- biosamples-web-core/src/main/java/uk/ac/ebi/spot/biosamples/model/solr/SolrIgnoredField.java
+++ biosamples-web-core/src/main/java/uk/ac/ebi/spot/biosamples/model/solr/SolrIgnoredField.java
@@ -6,7 +6,20 @@ import java.util.List;
 
 public enum SolrIgnoredField {
     SAMPLE(
-        "DerivedFrom_crt","DerivedFrom_crt_json"
+        "derivedFrom_crt",
+        "derivedFrom_crt_json",
+        "derivedTo_crt",
+        "derivedTo_crt_json",
+        "sameAs_crt",
+        "sameAs_crt_json",
+        "childOf_crt",
+        "childOf_crt_json",
+        "parentOf_crt",
+        "parentOf_crt_json",
+        "recuratedInto_crt",
+        "recuratedInto_crt_json",
+        "recuratedFrom_crt",
+        "recuratedFrom_crt_json"
     ),
 
     GROUP();
