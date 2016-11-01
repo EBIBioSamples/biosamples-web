c55cadaf49ce34503a43ab16657275311efbb4b8
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-11-01 15:29:10 +0000

    remove unused includes

--- biosamples-web/biosamples-web-core/src/main/java/uk/ac/ebi/spot/biosamples/repository/neo4j/NeoSampleRepository.java
+++ biosamples-web/biosamples-web-core/src/main/java/uk/ac/ebi/spot/biosamples/repository/neo4j/NeoSampleRepository.java
@@ -1,7 +1,5 @@
 package uk.ac.ebi.spot.biosamples.repository.neo4j;
 
-import org.springframework.data.neo4j.annotation.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import uk.ac.ebi.spot.biosamples.model.neo4j.NeoSample;
