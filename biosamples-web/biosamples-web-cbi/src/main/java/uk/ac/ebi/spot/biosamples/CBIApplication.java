d283e17f3a75cd77f5f4168211ff5841af0aef83
Kerruba <luca.cherubin@gmail.com>
2016-11-01 15:38:23 +0000

    Add the XML and JSON links to the single group/sample page

--- biosamples-web/biosamples-web-cbi/src/main/java/uk/ac/ebi/spot/biosamples/CBIApplication.java
+++ biosamples-web/biosamples-web-cbi/src/main/java/uk/ac/ebi/spot/biosamples/CBIApplication.java
@@ -2,7 +2,6 @@ package uk.ac.ebi.spot.biosamples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
